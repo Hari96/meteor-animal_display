9c3dc199f4c19452efd9db50e7d844528b5cd86b
Hari96 <bob@rgbassociates.co.uk>
2016-05-05 16:56:00 +0100

    Completed project by adding slideshow

--- js/animals.js
+++ js/animals.js
@@ -2,7 +2,7 @@ var animals_template, category_template, largeImage_template, slideshow_template
 
 // variables to store the current displayed animal and species
 var current_animal = animals_data.category[0];
-var current_species = current_animal.animals[0];
+var slideshow_animal = current_animal;
 
 // a helper function that instantiates a template
 // and displays the results in the content div
@@ -12,7 +12,7 @@ function showTemplate(template, data){
 }
 
 $(document).ready(function(){
-  
+
   // compile all of our templates ready for use
   var source   = $("#animals-template").html();
 	animals_template = Handlebars.compile(source);
@@ -23,8 +23,8 @@ $(document).ready(function(){
 	source   = $("#largeImg-template").html();
 	largeImg_template = Handlebars.compile(source);
 
-	//source   = $("#slideshow-template").html();
-//	slideshow_template = Handlebars.compile(source);
+	source   = $("#slideshow-template").html();
+  slideshow_template = Handlebars.compile(source);
 
   $("#animals-tab").click(function () {
 
@@ -37,8 +37,8 @@ $(document).ready(function(){
 		$(".animal-thumbnail").click(function (){
 			var index = $(this).data("id");
       current_animal = animals_data.category[index];
-
       showTemplate(category_template, current_animal);
+      slideshow_animal = current_animal; // makes sure slideshow has correct view
 
 			$(".animal-thumbnail").click(function (){
 				var index2 = $(this).data("id");
@@ -53,7 +53,7 @@ $(document).ready(function(){
     var index = $(this).data("id");
 		current_animal = animals_data.category[index];
 		showTemplate(category_template, current_animal);
-
+    slideshow_animal = current_animal; // makes sure slideshow has correct view
 		$(".animal-thumbnail").click(function (){
 			var index = $(this).data("id");
       current_animal = animals_data.category[0].animals[index];
@@ -66,7 +66,7 @@ $(document).ready(function(){
     var index = $(this).data("id");
 	  current_animal = animals_data.category[index];
 		showTemplate(category_template, current_animal);
-
+    slideshow_animal = current_animal; // makes sure slideshow has correct view
 		$(".animal-thumbnail").click(function (){
 			var index = $(this).data("id");
       current_animal = animals_data.category[1].animals[index];
@@ -79,13 +79,26 @@ $(document).ready(function(){
     var index = $(this).data("id");
 	  current_animal = animals_data.category[index];
 		showTemplate(category_template, current_animal);
-
+    slideshow_animal = current_animal; // makes sure slideshow has correct view
 		$(".animal-thumbnail").click(function (){
 			var index = $(this).data("id");
       current_animal = animals_data.category[2].animals[index];
 
       showTemplate(largeImg_template, current_animal);
 		});
+	});
+
+	$("#slideshow-tab").click(function () {
+		// display the slideshow template using the
+		// current animal
+		current_animal = slideshow_animal;
+		showTemplate(slideshow_template, current_animal);
+
+		// make the slideshow tab the active one
+		// first make the currently active tab inactive
+		$(".nav-tabs .active").removeClass("active");
+		// then make slideshow tab active
+		$("#slideshow-tab").addClass("active");
 	});
 
 	// start the page by showing the animals view
